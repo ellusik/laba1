4158ec60d2bc5cef8871d7faca10832101f7c2f1
ellusik <elina.chugunova@yandex.ru>
2025-09-29 12:51:44 +0300

    проверка чисел

--- laba1.cpp
+++ laba1.cpp
@@ -11,6 +11,16 @@ string station_name = "";
 int station_workshops = 0;
 int station_working = 0;
 
+bool checkNumber() {
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Error! Enter number: ";
+        return false;
+    }
+    return true;
+}
+
 void showMenu() {
     cout << "\n1. Add pipe\n2. Add station\n3. View all\n";
     cout << "4. Toggle pipe repair\n5. Edit workshops\n6. Exit\nChoice: ";
