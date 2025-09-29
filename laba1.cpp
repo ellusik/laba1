c873ac859baab6aea064ee02d1ae32fe35ccf00b
ellusik <elina.chugunova@yandex.ru>
2025-09-29 12:45:56 +0300

    менюшка

--- laba1.cpp
+++ laba1.cpp
@@ -10,3 +10,8 @@ bool pipe_repair = false;
 string station_name = "";
 int station_workshops = 0;
 int station_working = 0;
+
+void showMenu() {
+    cout << "\n1. Add pipe\n2. Add station\n3. View all\n";
+    cout << "4. Toggle pipe repair\n5. Edit workshops\n6. Exit\nChoice: ";
+}
